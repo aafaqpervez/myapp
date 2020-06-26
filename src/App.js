f3500d05c9dca7dc85e4ad9c310f17b25726f6a4
Aafaq Pervez <aafaqpervez@gmail.com>
2020-06-26 15:43:33 +0500

    complete assignment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import Dinner from './din.js';
 
+
 function App() {
   return (
     <div className="App">
-     <Dinner dishName="Nihari" sweetDish="kheer"/>
+    <Dinner dishName="Nihari" sweetDish="kheer"/>
 
-     {/* <hr/>
-     <Dinner dishName="Karahi" sweetDish="Gulab Jaman"/>
-     
-     <hr/>
+    <hr/>
+    <Dinner dishName="Karahi" sweetDish="Gulab Jaman"/>
+    
+    <hr/>
 
-     <Dinner dishName="Biryani" sweetDish="Ras Malai"/> */}
-     </div>
+    <Dinner dishName="Biryani" sweetDish="Ras Malai"/>
+    </div>
   );
 }
 
